9f6d0766aafea705a0f261ffa892caa8c3d18b9a
Michael H. Oshita <ijinpublic+github@gmail.com>
2018-01-19 03:59:15 +0900

    use time again & fix curl post

--- ecs/ecs_push_deploy_notify.sh
+++ ecs/ecs_push_deploy_notify.sh
@@ -9,25 +9,23 @@ export SHA1=`echo ${CIRCLE_SHA1} | cut -c1-7`
 # IDEA: put outside this function ref: http://www.savvyclutch.com/devops/continuous-deployment-to-aws-ecs-and-circle-ci/
 echo "pushing"
 docker tag $DOCKER_IMAGE:${DOCKER_TAG:-latest} $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/ci-rack:$SHA1
-docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/ci-rack:$SHA1
+time docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/ci-rack:$SHA1
 
 echo "deploying"
-./ecspresso deploy --config=$1
+time ./ecspresso deploy --config=$1
 
-echo "notifying"
 if [ $? -eq 0 ]; then
     export SL_COLOR="good"
     export SL_TEXT="Success: Deployed ${CIRCLE_BRANCH} (<${CIRCLE_COMPARE_URL}|${SHA1}>) by ${CIRCLE_USERNAME} !!"
-    export SL_ICON="https://www.cloudbees.com/sites/default/files/eleasticbeanstalk_square.png"
+    export SL_ICON="https://www.shippable.com/assets/images/logos/amazon-ecs.jpg"
     export EXIT=0
 else
     export SL_COLOR="danger"
     export SL_TEXT="Failure: Deploying ${CIRCLE_BRANCH} (<${CIRCLE_COMPARE_URL}|${SHA1}>) by ${CIRCLE_USERNAME} !!"
-    export SL_ICON="https://www.cloudbees.com/sites/default/files/eleasticbeanstalk_square.png"
+    export SL_ICON="https://www.shippable.com/assets/images/logos/amazon-ecs.jpg"
     export EXIT=1
 fi
 
-curl -X POST --data-urlencode 'payload={"username": "Elastic Beanstalk ('"$CIRCLE_PROJECT_REPONAME"')", "icon_url": "'"$SL_ICON"'", "channel": "'"${CHANNEL:-#test}"'", "a
-ttachments": [{ "color": "'"$SL_COLOR"'", "text": "'"$SL_TEXT"'", "mrkdwn_in": ["text"] }] }' https://hooks.slack.com/services/${SLACK_HOOK}
+curl -X POST --data-urlencode 'payload={"username": "ECS ('"$CIRCLE_PROJECT_REPONAME"')", "icon_url": "'"$SL_ICON"'", "channel": "'"${CHANNEL:-#test}"'", "attachments": [{ "color": "'"$SL_COLOR"'", "text": "'"$SL_TEXT"'", "mrkdwn_in": ["text"] }] }' https://hooks.slack.com/services/${SLACK_HOOK}
 
 exit $EXIT
