91a63e8b836c4d2e362786489e2fcac998674bd3
Michael H. Oshita <ijinpublic+github@gmail.com>
2018-01-19 02:36:35 +0900

    bug fix

--- ecs/ecs_push_deploy_notify.sh
+++ ecs/ecs_push_deploy_notify.sh
@@ -8,10 +8,21 @@ export SHA1=`echo ${CIRCLE_SHA1} | cut -c1-7`
 # TODO: multiple pushes
 # IDEA: put outside this function ref: http://www.savvyclutch.com/devops/continuous-deployment-to-aws-ecs-and-circle-ci/
 echo "pushing"
-docker tag $DOCKER_SOURCE_IMAGE:${DOCKER_TAG:-latest} $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/ci-rack:$SHA1
+docker tag $DOCKER_IMAGE:${DOCKER_TAG:-latest} $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/ci-rack:$SHA1
 time docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/ci-rack:$SHA1
 
 echo "deploying"
 time ./ecspresso deploy --config=$1
 
 echo "notifying"
+if [ $? -eq 0 ]; then
+    export SL_COLOR="good"
+    export SL_TEXT="Success: Deployed ${CIRCLE_BRANCH} (<${CIRCLE_COMPARE_URL}|${SHA1}>) by ${CIRCLE_USERNAME} !!"
+    export SL_ICON="https://www.cloudbees.com/sites/default/files/eleasticbeanstalk_square.png"
+else
+    export SL_COLOR="danger"
+    export SL_TEXT="Failure: Deploying ${CIRCLE_BRANCH} (<${CIRCLE_COMPARE_URL}|${SHA1}>) by ${CIRCLE_USERNAME} !!"
+    export SL_ICON="https://www.cloudbees.com/sites/default/files/eleasticbeanstalk_square.png"
+fi
+
+echo $SL_TEXT
